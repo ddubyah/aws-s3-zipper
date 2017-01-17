4d556af82f6e8afb9a189cbf5b5a56fccd864e54
Darren Wallace <wallace@midweekcrisis.com>
2017-01-17 15:42:28 +0000

    Made providing AWS credentials optional, so the AWS-SDK can default to using the global credentials object if available.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var s3 = require('s3');
 
 function S3Zipper(awsConfig) {
     assert.ok(awsConfig, 'AWS S3 options must be defined.');
-    assert.notEqual(awsConfig.accessKeyId, undefined, 'Requires S3 AWS Key.');
-    assert.notEqual(awsConfig.secretAccessKey, undefined, 'Requires S3 AWS Secret');
+    // assert.notEqual(awsConfig.accessKeyId, undefined, 'Requires S3 AWS Key.');
+    // assert.notEqual(awsConfig.secretAccessKey, undefined, 'Requires S3 AWS Secret.');
     assert.notEqual(awsConfig.region, undefined, 'Requires AWS S3 region.');
     assert.notEqual(awsConfig.bucket, undefined, 'Requires AWS S3 bucket.');
     this.init(awsConfig);
@@ -23,11 +23,14 @@ function listObjectInner() {
 S3Zipper.prototype = {
     init: function (awsConfig) {
         this.awsConfig = awsConfig;
-        AWS.config.update({
-            accessKeyId: awsConfig.accessKeyId,
-            secretAccessKey: awsConfig.secretAccessKey,
-            region: awsConfig.region
-        });
+				var configOptions = {
+					region: awsConfig.region
+				};
+				if(awsConfig.accessKeyId) configOptions.accessKey = awsConfig.accessKeyId;
+				if(awsConfig.secretAccessKey) configOptions.accessKey = awsConfig.secretAccessKey;
+
+        AWS.config.update(configOptions);
+				
         this.s3bucket = new AWS.S3({
             params: {
                 Bucket: this.awsConfig.bucket
